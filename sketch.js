8bd8de38649e1b131455ddcd508e4434f6549d0e
CaitlynP123 <74350761+CaitlynP123@users.noreply.github.com>
2021-02-13 16:30:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var balloon, balloonImg
+var backgroundImg
+var database, position
+var x,y
+
+function preload(){
+  balloonImg = loadAnimation("pro-C35 images/Hot Air Ballon-02.png",
+  "pro-C35 images/Hot Air Ballon-03.png","pro-C35 images/Hot Air Ballon-04.png")
+
+  backgroundImg = loadImage("pro-C35 images/Hot Air Ballon-01.png")
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+ 
+
+  balloon = createSprite(250, 250, 50, 50);
+  balloon.addAnimation("balloon", balloonImg)
+  balloon.scale = 0.5
+}
+
+function draw() {
+  background(backgroundImg); 
+  database = firebase.database() 
+
+  var balloonPosition = database.ref('balloon/position')
+  balloonPosition.on("value",readPosition,showError) 
+
+if(position!==undefined){
+  if(keyDown("up")){
+    balloon.y = balloon.y-10
+    updatePosition(0,-10)
+  } else if (keyDown("down")){
+    balloon.y = balloon.y+10
+    updatePosition(0,+10)
+  } else if (keyDown("right")){
+    balloon.x = balloon.x+10
+    updatePosition(+10,0)
+  } else if (keyDown("left")){
+    balloon.x = balloon.x-10
+    updatePosition(-10,0)
+  }  
+}
+  
+  drawSprites();
+}
+
+function updatePosition(x,y){
+  database.ref('balloon/position').set({
+    'x' : position.x+x,
+    'y' : position.y+y
+  })
+}
+
+function readPosition(data){
+  position = data.val()
+  balloon.x = position.x
+  balloon.y = position.y
+}
+
+function showError(){
+  console.log("E R R O R")
+}
